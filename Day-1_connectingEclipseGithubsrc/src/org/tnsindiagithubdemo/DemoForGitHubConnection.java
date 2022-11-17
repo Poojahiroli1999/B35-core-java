7d300cc99b6612d7ddc950590218df6f48715390
Admin <Admin@BYOD-661252>
2022-11-17 11:02:48 +0530

    First commit

---
+++ Day-1_connectingEclipseGithubsrc/src/org/tnsindiagithubdemo/DemoForGitHubConnection.java
@@ -0,0 +1,12 @@
+package org.tnsindiagithubdemo;
+
+public class DemoForGitHubConnection {
+// this demo is on connect eclipse
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println ("well come to TNS foundation");
+	}
+
+}
