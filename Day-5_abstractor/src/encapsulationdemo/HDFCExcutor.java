5d6f74bab4db05f33a47873e33af0edd7f4dc94d
Admin <Admin@BYOD-661252>
2022-11-25 09:24:27 +0530

    FIrst commit

---
+++ Day-5_abstractor/src/encapsulationdemo/HDFCExcutor.java
@@ -0,0 +1,10 @@
+package encapsulationdemo;
+
+public class HDFCExcutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day-5_abstractor/src/org/tnsindia/DefaultExcutor.java
@@ -0,0 +1,17 @@
+package org.tnsindia;
+
+public class DefaultExcutor {
+
+	public static void main(String[] args) {
+		
+			DefaultDemo d=new DefaultDemo(45);
+			d.display();
+			
+
+		}
+
+	
+
+	}
+
+
